aa4a1d5c0a4c3b434e3ace43d261bab6e1778ec5
Max Alva <alva85@gmail.com>
2018-07-17 22:35:44 -0500

    Usuario Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
@@ -14,7 +14,8 @@ import { AppRoutingModule } from './app-routing.module';
         BrowserModule,
         SharedModule,
         UsuariosModule,
-        AppRoutingModule
+        AppRoutingModule,
+        HttpClientModule
     ],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/usuarios/lista/lista.component.ts
+++ src/app/usuarios/lista/lista.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../services/usuario.service';
+import { Usuario } from '../../models/usuario.model';
 
 @Component({
-  selector: 'app-lista',
-  templateUrl: './lista.component.html',
-  styles: []
+    selector: 'app-lista',
+    templateUrl: './lista.component.html',
+    styles: []
 })
 export class ListaComponent implements OnInit {
 
-  constructor() { }
+    usuarios: Usuario[] = [];
 
-  ngOnInit() {
-  }
+    constructor(private usuarioService: UsuarioService) { }
+
+    ngOnInit() {
+        this.usuarioService.getUsers().subscribe(usuarios => {
+            this.usuarios = usuarios;
+            console.log(this.usuarios);
+        });
+    }
 
 }
