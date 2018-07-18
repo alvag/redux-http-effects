a5f454a30d3cac5d75addec00a0f6e4fb1ab40b4
Max Alva <alva85@gmail.com>
2018-07-17 23:18:42 -0500

    Appreducer, StoreModule y DevTools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { AppRoutingModule } from './app-routing.module';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { appReducers } from './store/app.reducer';
+import { environment } from '../environments/environment';
 
 @NgModule({
     declarations: [
@@ -15,7 +19,12 @@ import { AppRoutingModule } from './app-routing.module';
         SharedModule,
         UsuariosModule,
         AppRoutingModule,
-        HttpClientModule
+        HttpClientModule,
+        StoreModule.forRoot(appReducers),
+        StoreDevtoolsModule.instrument({
+            maxAge: 25,
+            logOnly: environment.production
+        })
     ],
     providers: [],
     bootstrap: [AppComponent]
