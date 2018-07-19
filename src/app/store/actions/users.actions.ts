91b0c82be6fbb3ffed29eedc66c67e010c99bcd5
Max Alva <alva85@gmail.com>
2018-07-18 22:59:49 -0500

    User Actions, Reducer y State

--- src/app/store/actions/users.actions.ts
+++ src/app/store/actions/users.actions.ts
@@ -18,7 +18,7 @@ export class LoadUsersFail implements Action {
 export class LoadUsersSuccess implements Action {
     readonly type = LOAD_USERS_SUCCESS;
 
-    constructor( public usuarios: User[] ) { }
+    constructor( public users: User[] ) { }
 }
 
 export type usersActions = LoadUsers | LoadUsersFail | LoadUsersSuccess;
